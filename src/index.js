adab43c424c4ea47103050861c08512e7e5865f8
MNdagu <ndagumark@gmail.com>
2024-07-20 21:00:14 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    // Retrieve the task description from the input field
+    const taskInput = document.getElementById("new-task-description");
+    const task = taskInput.value;
+
+    if (task) { 
+      const listItem = document.createElement('li');
+      listItem.textContent = task;
+      taskList.appendChild(listItem); 
+
+      taskInput.value = ''; 
+    }
+  });
+
+  let clear = document.getElementById('clear')
+  clear.addEventListener('click', () => {
+    taskList.innerHTML = '';
+  })
+
+
+
 });
